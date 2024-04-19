0f835ab7af8bdbda1eb1f4f68c7c18360250ef66
isabelamihai <isabelaelenamihai@gmail.com>
2024-04-19 10:29:53 +0000

    Deploying to gh-pages from @ isabelamihai/icecream-project@467c4442614836bb33b77fe0cd2525c2f12be399 🚀

---
+++ index.8bfc893c.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=window.location.href;document.querySelectorAll(".nav-item").forEach((function(n){n.href===e&&n.classList.add("active")}))}));
+//# sourceMappingURL=index.8bfc893c.js.map
